936957b99838f2ce1990aa8b3058239c255a326b
miaogaoliang <miaogaoliang@miaogaoliang.local>
2015-11-18 09:37:33 +0800

    Initial Commit

---
+++ NavgationViewControolerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavgationViewControolerDemo
+//
+//  Created by miaogaoliang on 15/11/18.
+//  Copyright © 2015年 miaogaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NavgationViewControolerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavgationViewControolerDemo
+//
+//  Created by miaogaoliang on 15/11/18.
+//  Copyright © 2015年 miaogaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
